147d604e6cb017279f7a16790715e7c3a077c078
camilocoelhogomes <camilo.coelho.gomes@gmail.com>
2022-11-04 07:12:18 -0300

    remove the other steps

--- src/Jobs/BatchJob.ts
+++ src/Jobs/BatchJob.ts
@@ -19,24 +19,6 @@ export class BatchJob extends Job {
       new Set([new CreatePerson()]),
       new OutputStream(namesFile)
     );
-    const secondStep = new Step(
-      fs.createReadStream(namesFile),
-      new Set([parse({delimiter: ';', columns: true})]),
-      new OutputStream('second_step.csv')
-    );
-    const thirdStep = new Step(
-      fs.createReadStream(namesFile),
-      new Set([parse({delimiter: ';', columns: true})]),
-      new OutputStream('third_step.csv')
-    );
-    const forthStep = new Step(
-      fs.createReadStream(namesFile),
-      new Set([parse({delimiter: ';', columns: true})]),
-      new OutputStream('forth_step.csv')
-    );
     this.steps.add(firstStep);
-    this.steps.add(secondStep);
-    this.steps.add(thirdStep);
-    this.steps.add(forthStep);
   }
 }
